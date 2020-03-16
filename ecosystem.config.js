5f673ec8ebc6a6da1f5f7328c9fa0434fe530a39
mbaxszy7 <390016746@qq.com>
2020-03-16 14:56:01 +0800

    feat(ecosystem): add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  apps: [
+    {
+      name: "pika",
+      script: "./src/server/index.js",
+
+      // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+      args: "one two",
+      instances: 2,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: "root",
+      host: "111.229.9.30",
+      ref: "origin/master",
+      repo: "git@github.com:mbaxszy7/music-motion.git",
+      path: "/var/www/production",
+      "post-deploy":
+        "npm install && npm run build && pm2 reload ecosystem.config.js --env production",
+    },
+  },
+}
